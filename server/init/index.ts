37eea51935d68bc4921182591e51b9332b717ff9
MrWangJustToDo <2711470541@qq.com>
2021-11-02 22:54:22 +0800

    remove log

--- server/init/index.ts
+++ server/init/index.ts
@@ -1,11 +1,11 @@
 import e from "express";
-import { decodeURI, initDBConnect, initSession, initUser, serverLog } from "./init";
+import { decodeURI, initDBConnect, initSession, initUser, /* serverLog */ } from "./init";
 
 const init = (expressApp: e.Express) => {
   expressApp.use(decodeURI);
   expressApp.use(initDBConnect);
   expressApp.use(initSession);
-  expressApp.use(serverLog);
+  // expressApp.use(serverLog);
   expressApp.use(initUser);
 };
 
