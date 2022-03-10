9203b799ec932e17841055b277cede1b462839ae
MrWangJustToDo <2711470541@qq.com>
2022-03-10 11:40:38 +0800

    update

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -11,15 +11,6 @@ export const Head = ({ title = "Blog" }: { title?: string }) => {
       <meta name="description" content="this is my next blog" />
       <title>{title} | Blog</title>
       <link rel="icon" href="/favicon.ico" />
-      {/* 字体图标 */}
-      <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
-      {/* bootstrap */}
-      <link href="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" rel="stylesheet" />
-      {/* animate css */}
-      <link href="https://cdn.bootcdn.net/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet" />
-      {/* react slick */}
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
     </OriginalHead>
   );
 };
